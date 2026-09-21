ce0c577241d5034f9089e4f9cc0b7779e58b6822
dezlymacauley <dezlymacauley@gmail.com>
2026-09-21 18:17:52 +0200

    Guide on rendering html content

---
+++ d03-external-dependencies/d02-frameworks/d01-sveltekit/d01-svelte-component-syntax/src/routes/d01-basic-svelte/d01-introduction/d06-html-tags/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let htmlContent: string = `
+    <h2>Welcome to my website!</h2>
+    <p>This is a <strong>simple example</strong> of HTML inside a string.</p>
+  `;
+</script>
+
+<!--
+  Only use `@html` to render internal content that is secure.
+  Do not use it to render content submitted by the user,
+  or content from an external source.
+-->
+<p>{@html htmlContent}</p>
